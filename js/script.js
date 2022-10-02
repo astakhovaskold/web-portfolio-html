11c75671c894ebd960a7bb54e203ceb899ef996c
Askold Astakhov <astahov@element-soft.com>
2022-10-02 20:15:51 +0300

    Hamburger menu + overlay

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@
 document.querySelector('.hamburger').addEventListener('click', function (e) {
     const hamburger = e.currentTarget;
     const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const overlay = document.querySelector('.overlay');
 
     hamburger.classList.toggle('open');
     hamburgerMenu.classList.toggle('open');
+    overlay.classList.toggle('open');
 })
