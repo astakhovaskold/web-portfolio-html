9c11e944daf3b6acd18da6c07700a8bc4dcc4dfd
Askold Astakhov <astahov@element-soft.com>
2022-10-05 21:44:07 +0300

    Contact Form + Modal + Form Validation Starter

--- js/script.js
+++ js/script.js
@@ -5,9 +5,40 @@
 document.querySelector('.hamburger').addEventListener('click', function (e) {
     const hamburger = e.currentTarget;
     const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const overlay = document.querySelector('.overlay');
+    const overlay = hamburgerMenu.closest('.overlay');
+
+    overlay.classList.toggle('open');
 
     hamburger.classList.toggle('open');
     hamburgerMenu.classList.toggle('open');
-    overlay.classList.toggle('open');
+})
+
+document.querySelector('[data-modal="open"]').addEventListener('click', function (e) {
+    const button = e.currentTarget;
+
+    const modal = document.querySelector(button.dataset.target);
+    const overlay = modal.closest('.overlay');
+
+    overlay.classList.add('open');
+    modal.classList.add('open');
+})
+
+document.querySelector('[data-modal="close"]').addEventListener('click', function (e) {
+    const button = e.currentTarget;
+
+    const modal = button.closest('.modal');
+    const overlay = button.closest('.overlay');
+
+    overlay.classList.remove('open');
+    modal.classList.remove('open');
+})
+
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const form = e.target;
+
+    if (form.checkValidity()) {
+        form.submit();
+    }
 })
